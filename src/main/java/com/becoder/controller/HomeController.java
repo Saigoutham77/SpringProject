502ae2ada51c6fa0fe95aba0f058e952620f58e2
nuthankumarsahukara <142878837+nuthankumarsahukara@users.noreply.github.com>
2024-02-19 17:44:30 +0530

    Update HomeController.java

--- src/main/java/com/becoder/controller/HomeController.java
+++ src/main/java/com/becoder/controller/HomeController.java
@@ -57,7 +57,7 @@ public class HomeController {
 			session.setAttribute("msg", "something wrong on server");
 		}
 
-		return "redirect:/SpringProject/loadEmpSave";
+		return "redirect:/loadEmpSave";
 	}
 
 	@PostMapping("/SpringProject/updateEmpDtls")
@@ -74,7 +74,7 @@ public class HomeController {
 			session.setAttribute("msg", "something wrong on server");
 		}
 
-		return "redirect:/SpringProject";
+		return "redirect:/";
 	}
 
 	@GetMapping("/SpringProject/deleteEmp/{id}")
@@ -85,7 +85,7 @@ public class HomeController {
 		} else {
 			session.setAttribute("msg", "something wrong on server");
 		}
-		return "redirect:/SpringProject";
+		return "redirect:/";
 	}
 
 }
